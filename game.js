7de89ca4f93dc968fbc1406b010f227f42eb1e02
jackieooikk <98008869+jackieooikk@users.noreply.github.com>
2023-11-13 16:35:46 +0800

    finished simon game with functionality to end and restart game

--- game.js
+++ game.js
@@ -1,16 +1,52 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
-
 var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+
+$(document).keypress(function() {
+    if (!started) {
+        nextSequence();
+        started = true;
+    }
+})
 
 $(".btn").click(function() {
     var userChosenColour = $(this).attr("id");
 
     userClickedPattern.push(userChosenColour);
+
     playSound(userChosenColour);
     animatePress(userChosenColour);
+
+    checkAnswer(userClickedPattern.length - 1);
+
 });
+
+function checkAnswer(currentLevel) {
+
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+        if (userClickedPattern.length == gamePattern.length) {
+            setTimeout(() => {
+                nextSequence();
+            }, 1000)
+        }
+    } else {
+        playSound("wrong");
+
+        $("#level-title").text("Game over, press any key to restart");
+        $("body").addClass("game-over");
+
+        setTimeout(() => {
+            $("body").removeClass("game-over");
+        }, 200)
+        startOver();
+    }
+
+}
 
 function animatePress(currentColor) {
     $("#" + currentColor).addClass("pressed");
@@ -20,12 +56,22 @@ function animatePress(currentColor) {
 }
 
 function nextSequence() {
+    userClickedPattern = [];
+
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColour)
+    level++;
+    $("#level-title").text("level " + level);
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
 }
 
 function playSound(name) {
